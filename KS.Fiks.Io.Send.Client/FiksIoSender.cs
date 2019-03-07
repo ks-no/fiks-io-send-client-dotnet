754a3d7f73cd22f61431852f9aa6536e1ca901a9
Sveinung Fjær <sveinung.fjaer@webstep.no>
2019-03-07 10:31:25 +0100

    Do not set boundary explicitly. It should not be needed

--- KS.Fiks.Io.Send.Client/FiksIoSender.cs
+++ KS.Fiks.Io.Send.Client/FiksIoSender.cs
@@ -73,7 +73,7 @@ namespace KS.Fiks.Io.Send.Client
 
             var dataContent = new StreamContent(data);
 
-            var request = new MultipartFormDataContent(Guid.NewGuid().ToString());
+            var request = new MultipartFormDataContent();
             request.Add(stringContent, "metadata");
             request.Add(dataContent, "data", Guid.NewGuid().ToString());
             System.Console.WriteLine("----MultipartFromDataContent----");
