4ce00439829e0cc9d84f3b274fda58859ea8fcad
Jarle Børsheim <jarle.borsheim@gmail.com>
2022-01-26 19:02:46 +0100

    La til klientMeldingId på SendtMeldingApiModel

--- KS.Fiks.IO.Send.Client/Models/SendtMeldingApiModel.cs
+++ KS.Fiks.IO.Send.Client/Models/SendtMeldingApiModel.cs
@@ -9,6 +9,9 @@ namespace KS.Fiks.IO.Send.Client.Models
         [JsonProperty("meldingId")]
         public Guid MeldingId { get; set; }
 
+        [JsonProperty("klientMeldingID")]
+        public Guid? KlientMeldingID { get; }
+
         [JsonProperty("meldingType")]
         public string MeldingType { get; set; }
 
